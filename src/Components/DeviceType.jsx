cc3646c47ba096118615bb9483a23308b7846b14
habibFrh <habibellah.ferhi@univ-constantone2.dz>
2024-04-14 22:18:42 +0100

    games

--- src/Components/DeviceType.jsx
+++ src/Components/DeviceType.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Cards from "./TeamCard"
+import MozillianRacer from "../assets/mozillian-racer.png"
+import Chess from "../assets/chess.png"
+import Flappy from "../assets/flappy.png"
 
 const DeviceType = () => {
   return (
@@ -9,10 +12,9 @@ const DeviceType = () => {
         <h2 className="text-3xl font-bold mb-8">Mozilla Games</h2>
         <p className="mb-8 font-bold">developed by the web dev team !</p>
         <div className="max-w-[900px] mx-auto grid md:grid-cols-2 gap-8 items-center">
-          <Cards animation={"flip-left"} team={"Mozillian Racer"} img={null} />
-          <Cards animation={"flip-left"} team={"Mobile Development"} img={null} />
-          <Cards animation={"flip-left"} team={"Network / Security"} img={null} />
-          <Cards animation={"flip-left"} team={"Artificial Intelligence / ML"} img={null} />
+          <a href="https://habibfrh.github.io/Mozillian-Racer-fox-Racer-/"><Cards animation={"flip-left"} team={"Mozillian Racer"} img={MozillianRacer} /></a>
+          <a href="https://habibfrh.github.io/flappy-fox/"><Cards animation={"flip-left"} team={"Flappy Fox"} img={Flappy} /></a>
+          <a href="https://habibfrh.github.io/chess-game/"><Cards animation={"flip-left"} team={"Chess game"} img={Chess} /></a>
         </div>
       </div>
 
